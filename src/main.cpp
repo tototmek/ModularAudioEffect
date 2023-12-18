3b48305b2d92634fb6c26c88bbf72e38b0117661
Tomek Zebrowski <zebromek@gmail.com>
2023-12-18 14:45:39 +0100

    Add script to run clang-tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "blocks/gain.h"
+#include "blocks/core.h"
 #include "project_info.h"
 #include <spdlog/spdlog.h>
 
@@ -8,6 +8,25 @@ int main() {
     blocks::Gain gain{2.0f};
     float input = 1.0f;
     float output = gain.process(input);
+    spdlog::info("Gain test");
     spdlog::info("{} -> {}", input, output);
+
+    blocks::Delay delay(3.0f / blocks::kSampleRate);
+    spdlog::info("Delay test");
+    input = 1.0f;
+    output = delay.process(input);
+    spdlog::info("{} -> {}", input, output);
+    input = 2.0f;
+    output = delay.process(input);
+    spdlog::info("{} -> {}", input, output);
+    input = 3.0f;
+    output = delay.process(input);
+    spdlog::info("{} -> {}", input, output);
+    for (size_t i = 0; i < 10; ++i) {
+        input = 0.0f;
+        output = delay.process(input);
+        spdlog::info("{} -> {}", input, output);
+    }
+
     return 0;
 }
---
+++ utils/run_clang_tidy.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+files=0
+failed=0
+RED='\033[1;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo "Files to check:"
+# changed_files=`git --no-pager diff origin/master --stat --color=always | grep -v dependencies/`
+changed_files=$(find . | grep -e "src/.*\\.cpp$")
+echo "$changed_files"
+for file in $changed_files;
+do
+  if [ ! -f $file ]; then
+    continue
+  fi
+  echo    ""
+  echo -e "Checking file: ${GREEN}$file${NC}"
+  output=`clang-tidy-12 $file --quiet -p ./build 2>/dev/null`
+  status=$?
+  echo -n "${output}"
+  let "failed+=$status"
+  let "files+=1"
+  if [[ "$status" -eq 0 ]]; then
+    echo -e " Status: ${GREEN}Passed${NC}"
+  else
+    echo -e " Status: ${RED}Failed${NC}"
+  fi
+done
+
+let "passed = $files - $failed"
+echo ""
+echo "$passed/$files files passed the check."
+echo "Exit status $failed"
+exit $failed
+
+
+    
